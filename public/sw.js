16d329afb96b420816535fe8b4b752449f396498
rccgnsparepo-hash <rccgnsparepo@gmail.com>
2026-04-18 15:41:27 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+<!-- PushAlert Unified Code -->
+<script type="text/javascript">
+    (function(d, t) {
+        var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+        g.src = "https://cdn.pushalert.co/unified_27d2c84087fdc6069e526746a5e3d959.js";
+        s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+</script>
+<!-- End PushAlert Unified Code -->
